a57f3dadc43e4a001c703c7650747fba99a592d5
Ramadhirra <107428291+aridoutofrange@users.noreply.github.com>
2023-06-14 06:44:29 +0700

    Update SpeculativeNet.py

--- SpeculativeNet.py
+++ SpeculativeNet.py
@@ -1,53 +1,54 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.utils import to_categorical
 import tensorflow as tf
 
-# Define the neural network architecture
-def branch_prediction_net(input_shape):
-    model = tf.keras.Sequential([
-        tf.keras.layers.Input(shape=input_shape),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(64, activation='relu'),
-        tf.keras.layers.Dense(2, activation='softmax')
-    ])
-    return model
+# Define the input shape
+input_shape = (num_features,)
 
-# Define the loss function and optimizer
-loss_fn = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
-optimizer = tf.keras.optimizers.Adam()
+# Define the model architecture
+model = Sequential()
+model.add(Dense(128, activation='relu', input_shape=input_shape))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
 
-# Define the training loop
-@tf.function
-def train_step(inputs, labels, model):
-    with tf.GradientTape() as tape:
-        # Forward pass
-        logits = model(inputs, training=True)
-        # Compute the loss
-        loss = loss_fn(labels, logits)
-    # Compute the gradients
-    gradients = tape.gradient(loss, model.trainable_variables)
-    # Update the weights
-    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-    # Compute the accuracy
-    accuracy = tf.reduce_mean(tf.keras.metrics.categorical_accuracy(labels, logits))
-    return loss, accuracy
+# Compile the model
+optimizer = Adam(lr=learning_rate)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
-# Train the model
-def train_model(model, train_dataset, validation_dataset, epochs):
-    for epoch in range(epochs):
-        # Train on the training dataset
-        train_loss = tf.keras.metrics.Mean()
-        train_accuracy = tf.keras.metrics.Mean()
-        for inputs, labels in train_dataset:
-            loss, accuracy = train_step(inputs, labels, model)
-            train_loss(loss)
-            train_accuracy(accuracy)
-        # Validate on the validation dataset
-        val_loss = tf.keras.metrics.Mean()
-        val_accuracy = tf.keras.metrics.Mean()
-        for inputs, labels in validation_dataset:
-            logits = model(inputs, training=False)
-            loss = loss_fn(labels, logits)
-            val_loss(loss)
-            val_accuracy(tf.reduce_mean(tf.keras.metrics.categorical_accuracy(labels, logits)))
-        # Print the results
-        print(f'Epoch {epoch+1}, Train Loss: {train_loss.result()}, Train Accuracy: {train_accuracy.result()}, Val Loss: {val_loss.result()}, Val Accuracy: {val_accuracy.result()}')
+# Define callbacks for early stopping and model checkpointing
+early_stopping = EarlyStopping(monitor='val_loss', patience=10)
+model_checkpoint = ModelCheckpoint(filepath='best_model.h5', monitor='val_loss', save_best_only=True)
+
+# Train the model with pruning
+pruning_schedule = tfmot.sparsity.keras.PolynomialDecay(initial_sparsity=0.50, final_sparsity=0.90, begin_step=1000, end_step=x_train.shape[0], frequency=100)
+prune_low_magnitude = tfmot.sparsity.keras.prune_low_magnitude
+model_for_pruning = prune_low_magnitude(model, pruning_schedule=pruning_schedule)
+model_for_pruning.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+model_for_pruning.fit(x_train, y_train, batch_size=batch_size, epochs=num_epochs, validation_data=(x_val, y_val), callbacks=[early_stopping, model_checkpoint])
+
+# Load the best model from model checkpoint
+best_model = tf.keras.models.load_model('best_model.h5')
+
+# Quantize the model
+converter = tf.lite.TFLiteConverter.from_keras_model(best_model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+tflite_model = converter.convert()
+
+# Save the quantized model
+with open('quantized_model.tflite', 'wb') as f
+    f.write(tflite_model)
+
+# Train the model with distributed computing
+strategy = tf.distribute.MirroredStrategy()
+with strategy.scope()
+    distributed_model = tf.keras.models.clone_model(best_model)
+    distributed_model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    distributed_model.fit(x_train, y_train, batch_size=batch_size, epochs=num_epochs, validation_data=(x_val, y_val))
+
+# Evaluate the model
+score = distributed_model.evaluate(x_test, y_test, verbose=0)
